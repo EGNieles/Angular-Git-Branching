98d6a1aab324d7deb9c8878ca111014a56b2ee0f
Jae Anne Dientes <dientesjam97@gmail.com>
2018-01-10 13:02:52 +0800

    new commit

---
+++ Desktop/Angular-Git-Branching-master/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+   AppComponent,
+  HighlightDirective,
+  TitleComponent,
+
+  AwesomePipe,
+  ContactComponent,
+  ContactHighlightDirective
+  ],
+  imports: [ BrowserModule, FormsModule ],
+  
+  providers: [ContactService, UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
